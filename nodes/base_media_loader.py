cea9549c72042d66d5a92e8a2bda8d10463d0f76
Emmanuel Prat <eprat@deepiks.io>
2026-03-23 16:27:07 +0100

    fix: resolve missing media positional argument in VALIDATE_INPUTS for LoadImage and LoadVideo nodes
    Signed-off-by: Emmanuel Prat <eprat@deepiks.io>

--- nodes/base_media_loader.py
+++ nodes/base_media_loader.py
@@ -3,7 +3,10 @@ import hashlib
 
 class BaseMediaLoaderNode:
     @classmethod
-    def IS_CHANGED(cls, media):
+    def IS_CHANGED(cls, **kwargs):
+        media = kwargs.get("image") or kwargs.get("video")
+        if not media:
+            return ""
         media_path = folder_paths.get_annotated_filepath(media)
         m = hashlib.sha256()
         with open(media_path, 'rb') as f:
@@ -11,7 +14,10 @@ class BaseMediaLoaderNode:
         return m.digest().hex()
 
     @classmethod
-    def VALIDATE_INPUTS(cls, media):
+    def VALIDATE_INPUTS(cls, **kwargs):
+        media = kwargs.get("image") or kwargs.get("video")
+        if not media:
+            return "Missing media input"
         if not folder_paths.exists_annotated_filepath(media):
             return "Invalid media file: {}".format(media)
         return True
